ca6c5a86a428adfd04b67af02b34432042d75d54
zhouchaowen <zhouchaowen@zdns.cn>
2022-08-21 17:35:16 +0800

    lesson 12:add context trace

--- bussiness/web/mid/logger.go
+++ bussiness/web/mid/logger.go
@@ -3,28 +3,37 @@ package mid
 import (
 	"context"
 	"github.com/Zhouchaowen/ultimate-service/foundation/web"
-	"go.uber.org/zap"
 	"net/http"
 	"time"
+
+	"go.uber.org/zap"
 )
 
-// Logger ...
+// Logger writes some information about the request to the logs in the
+// format: TraceID : (200) GET /foo -> IP ADDR (latency)
 func Logger(log *zap.SugaredLogger) web.Middleware {
+
+	// This is the actual middleware function to be executed.
 	m := func(handler web.Handler) web.Handler {
 
+		// Create the handler that will be attached in the middleware chain.
 		h := func(ctx context.Context, w http.ResponseWriter, r *http.Request) error {
-			traceId := "000000000000000000"
-			statusCode := http.StatusOK
-			now := time.Now()
 
-			log.Infow("request started", "traceid", traceId, "method", r.Method, "path", r.URL.Path,
+			// If the context is missing this value, request the service
+			// to be shutdown gracefully.
+			v, err := web.GetValues(ctx)
+			if err != nil {
+				return err //web.NewShutdownError("web value missing from context")
+			}
+
+			log.Infow("request started", "traceid", v.TraceID, "method", r.Method, "path", r.URL.Path,
 				"remoteaddr", r.RemoteAddr)
 
 			// Call the next handler.
-			err := handler(ctx, w, r)
+			err = handler(ctx, w, r)
 
-			log.Infow("request completed", "traceid", traceId, "method", r.Method, "path", r.URL.Path,
-				"remoteaddr", r.RemoteAddr, "statuscode", statusCode, "since", time.Since(now))
+			log.Infow("request completed", "traceid", v.TraceID, "method", r.Method, "path", r.URL.Path,
+				"remoteaddr", r.RemoteAddr, "statuscode", v.StatusCode, "since", time.Since(v.Now))
 
 			// Return the error so it can be handled further up the chain.
 			return err
@@ -32,5 +41,6 @@ func Logger(log *zap.SugaredLogger) web.Middleware {
 
 		return h
 	}
+
 	return m
 }
