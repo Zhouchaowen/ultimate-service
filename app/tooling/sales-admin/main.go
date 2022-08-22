05f8c949f2b30601db5bc65a356a4deb609c5c91
zhouchaowen <zhouchaowen@zdns.cn>
2022-08-22 14:38:33 +0800

    lesson 17:add generate token and validate token and parse token

--- app/tooling/sales-admin/main.go
+++ app/tooling/sales-admin/main.go
@@ -5,16 +5,139 @@ import (
 	"crypto/rsa"
 	"crypto/x509"
 	"encoding/pem"
+	"errors"
 	"fmt"
+	"io"
 	"os"
+	"time"
+
+	"github.com/golang-jwt/jwt/v4"
 )
 
 func main() {
-	err := GenKey()
+	err := GenToken()
 	if err != nil {
 		fmt.Println(err)
 		os.Exit(1)
 	}
+}
+
+// GenToken generates a JWT for the specified user.
+func GenToken() error {
+	file, err := os.Open("zarf/keys/54bb2165-71e1-41a6-af3e-7da4a0e1e2c1.pem")
+	if err != nil {
+		return err
+	}
+
+	// limit PEM file size to 1 megabyte. This should be reasonable for
+	// almost any PEM file and prevents shenanigans like linking the file
+	// to /dev/random or something like that.
+	privatePEM, err := io.ReadAll(io.LimitReader(file, 1024*1024))
+	if err != nil {
+		return fmt.Errorf("reading auth private key: %w", err)
+	}
+
+	privateKey, err := jwt.ParseRSAPrivateKeyFromPEM(privatePEM)
+	if err != nil {
+		return fmt.Errorf("parsing auth private key: %w", err)
+	}
+
+	//================================================================
+
+	// Generating a token requires defining a set of claims. In this applications
+	// case, we only care about defining the subject and the user in question and
+	// the roles they have on the database. This token will expire in a year.
+	//
+	// iss (issuer): Issuer of the JWT
+	// sub (subject): Subject of the JWT (the user)
+	// aud (audience): Recipient for which the JWT is intended
+	// exp (expiration time): Time after which the JWT expires
+	// nbf (not before time): Time before which the JWT must not be accepted for processing
+	// iat (issued at time): Time at which the JWT was issued; can be used to determine age of the JWT
+	// jti (JWT ID): Unique identifier; can be used to prevent the JWT from being replayed (allows a token to be used only once)
+	claims := struct {
+		jwt.StandardClaims
+		Roles []string
+	}{
+		StandardClaims: jwt.StandardClaims{
+			Subject:   "123456789",
+			Issuer:    "service project",
+			ExpiresAt: time.Now().UTC().Add(8760 * time.Hour).Unix(),
+			IssuedAt:  time.Now().UTC().Unix(),
+		},
+		Roles: []string{"ADMIN"},
+	}
+
+	method := jwt.GetSigningMethod("RS256")
+	token := jwt.NewWithClaims(method, claims)
+	token.Header["kid"] = "54bb2165-71e1-41a6-af3e-7da4a0e1e2c1"
+
+	tokenStr, err := token.SignedString(privateKey)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("-----BEGIN TOKEN-----\n%s\n-----END TOKEN-----\n", tokenStr)
+
+	//================================================================
+	// Marshal the public key from the private key to PKIX.
+	asn1Bytes, err := x509.MarshalPKIXPublicKey(&privateKey.PublicKey)
+	if err != nil {
+		return fmt.Errorf("marshaling public key: %w", err)
+	}
+
+	// Create a file for the public key information in PEM form.
+	publicFile, err := os.Create("public.pem")
+	if err != nil {
+		return fmt.Errorf("creating public file: %w", err)
+	}
+	defer publicFile.Close()
+
+	// Construct a PEM block for the public key.
+	publicBlock := pem.Block{
+		Type:  "RSA PUBLIC KEY",
+		Bytes: asn1Bytes,
+	}
+	// Write the public key to the public key file.
+	if err := pem.Encode(publicFile, &publicBlock); err != nil {
+		return fmt.Errorf("encoding to public file: %w", err)
+	}
+
+	//================================================================
+	// parser token
+	parser := jwt.Parser{
+		ValidMethods: []string{"RS256"},
+	}
+
+	var parsedClaims struct {
+		jwt.StandardClaims
+		Roles []string
+	}
+
+	keyFunc := func(t *jwt.Token) (interface{}, error) {
+		kid, ok := t.Header["kid"]
+		if !ok {
+			return nil, errors.New("missing key id (kid) in token header")
+		}
+		kidID, ok := kid.(string)
+		if !ok {
+			return nil, errors.New("user token key id (kid) must be string")
+		}
+		fmt.Println("KID", kidID)
+		return &privateKey.PublicKey, nil
+	}
+
+	parsedToken, err := parser.ParseWithClaims(tokenStr, &parsedClaims, keyFunc)
+	if err != nil {
+		return fmt.Errorf("parsing token: %w", err)
+	}
+
+	if !parsedToken.Valid {
+		return errors.New("invalid token")
+	}
+
+	fmt.Println("Token Validated")
+	return nil
 }
 
 // GenKey creates an x509 private/public key for auth tokens.
--- go.mod
+++ go.mod
@@ -5,7 +5,8 @@ go 1.17
 require (
 	github.com/ardanlabs/conf/v3 v3.1.2
 	github.com/dimfeld/httptreemux/v5 v5.4.0
-	go.opentelemetry.io/otel v1.9.0
+	github.com/golang-jwt/jwt/v4 v4.4.2
+	go.opentelemetry.io/otel/trace v1.9.0
 	go.uber.org/automaxprocs v1.5.1
 	go.uber.org/zap v1.21.0
 )
@@ -14,7 +15,7 @@ require (
 	github.com/go-logr/logr v1.2.3 // indirect
 	github.com/go-logr/stdr v1.2.2 // indirect
 	github.com/stretchr/testify v1.7.1 // indirect
-	go.opentelemetry.io/otel/trace v1.9.0 // indirect
+	go.opentelemetry.io/otel v1.9.0 // indirect
 	go.uber.org/atomic v1.7.0 // indirect
 	go.uber.org/multierr v1.6.0 // indirect
 )
